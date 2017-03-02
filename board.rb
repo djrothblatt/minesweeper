625903cd679672331ddd1a033a55166a67b14725
Edward Graham <ejpgraham@gmail.com>
2017-03-02 17:45:13 -0500

    Populate board

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative "tile"
+
 class Board
   BOARD_SIZE = 9
 
@@ -5,6 +7,14 @@ class Board
 
   def initialize
     @grid = Array.new(BOARD_SIZE){ Array.new(BOARD_SIZE) }
+  end
+
+  def populate
+    grid.each_with_index do |row, i|
+      row.each_with_index do |col, j|
+        self.grid[i][j] = Tile.new([i,j], self)
+      end
+    end
   end
 
 end
